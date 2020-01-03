a56b5ba5406eca92a60fdf985955362339774986
Marek Urbaniec <marek.urbaniec1@gmail.com>
2020-01-03 23:55:54 +0100

    Seanse endpoint

--- server/models/movie-model.js
+++ server/models/movie-model.js
@@ -10,8 +10,9 @@ const Movie = new Schema(
         time: {type: String, require: true },
         imgSrc:{type:String, required: true},
         seanses:[{
+            hours: [{type: String, required:true}],
             day: {type :String, required: true},
-            hours: [{type: String, required:true}]
+            
         }]
         
     },
